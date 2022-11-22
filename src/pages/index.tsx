55d09a2c2c2b243d7c7d92f654f67fe6e805903d
lionet <jialecode@qq.com>
2022-11-22 15:19:41 +0800

    chore: 修理文档

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="DCT 使用文档">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
